f3992cc9178cdf0899f8f2f2e6c5ccecc181f4df
Aaron M Barnes <barnes88@purdue.edu>
2021-01-05 22:20:57 -0500

    add timestamps

--- run_bench.py
+++ run_bench.py
@@ -2,6 +2,7 @@
 
 from subprocess import run
 import os
+from datetime import datetime
 
 queries = [*range(1, 23)]
 path = "./nvprof_TPCH/"
@@ -9,10 +10,11 @@ if not os.path.isdir(path):
     os.makedirs(path)
 for query in queries:
     print("Running Query q" + str(query))
+    print("Started at " + datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
     filename = path + str(query) + ".txt"
     f = open(filename, "w")
     try:
-        output = run(["nvprof --profile-child-processes --metrics ipc --csv --log-file Gpu_Tpch_%p_q"+str(query)+".csv --profile-from-start off ./sbt_launch.sh Gpu_Tpch "+ str(query)+" "+str(query)], stdout=f, shell=True, timeout=24*60*60)
+        output = run(["nvprof --profile-child-processes --events elapsed_cycles_sm,branch --metrics ipc,l2_l1_read_hit_rate,ldst_fu_utilization --csv --log-file Gpu_Tpch_q"+str(query)+"_%p.csv --profile-from-start off ./sbt_launch.sh Gpu_Tpch "+ str(query)+" "+str(query)], stdout=f, shell=True, timeout=24*60*60)
     except:
         print("QUERY " + str(query) +" failed")
     finally:
